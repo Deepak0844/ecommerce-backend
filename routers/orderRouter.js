d9c418e66d3f84efc618988cf08be42962cff8a6
Deepak0844 <deepakram0105@gmail.com>
2022-02-14 00:08:20 +0530

    .

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -43,7 +43,7 @@ router.delete("/:id", isAdmin, async (request, response) => {
 });
 
 //get order by user
-router.get("/find/:userId", authorization, async (request, response) => {
+router.get("/find/:userId", verifyToken, async (request, response) => {
   const orderByUser = await Order.find({ userId: request.params.userId });
   response.send(orderByUser);
 });
